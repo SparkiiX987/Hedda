ceb2f71123cc67e23362370b9e3a8fa9be1204aa
0shiros <133508599+0shiros@users.noreply.github.com>
2025-05-20 10:48:40 +0200

    Feat/weapon class (#4)
    * Create weapon class
    
    Create weapon class with all variables
    
    * Add bullet number as variable
    
    * Create DataAssets of each Weapon
    
    Create DataAssets for the 4 current weapon + set variable in accordance to the Weapon List

---
+++ Source/Hedda/Public/Weapon.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "NiagaraSystem.h"
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "Weapon.generated.h"
+
+UENUM(BlueprintType)
+enum class EWeaponType : uint8
+{
+	LightArtillery	UMETA(DisplayName = "Light Artillery"),
+	HeavyArtillery	UMETA(DisplayName = "Heavy Artillery"),
+};
+
+UCLASS()
+class HEDDA_API UWeapon : public UDataAsset
+{
+	GENERATED_BODY()
+	
+	public : 
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+		FString weaponName;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+		EWeaponType weaponType;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+		float damage;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+		float attackSpeed;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+		float reloadTime;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+		int bulletNumber;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+		int ammo;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Effects")
+		UNiagaraSystem* muzzleParticle;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+		AActor* projectile;
+};
