df2b4a6fab140fe3cebf046af5c1007ea02085d6
Hibizea1 <m.raynaud@lahorde.school>
2025-05-26 17:43:53 +0200

    Fixing AI

---
+++ Source/Hedda/Private/BTC_SelectRandom.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTC_SelectRandom.h"
+
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTC_SelectRandom::UBTC_SelectRandom()
+{
+	NodeName = "Random Choice 50/50";
+}
+
+int32 UBTC_SelectRandom::GetNextChildHandler(FBehaviorTreeSearchData& SearchData, int32 PrevChild, EBTNodeResult::Type LastResult) const
+{
+	if (PrevChild == INDEX_NONE)
+	{
+		// 1 chance sur 2 : 0 ou 1
+		const int32 RandomIndex = FMath::RandRange(0, 1);
+		return FMath::Clamp(RandomIndex, 0, GetChildrenNum() - 1);
+	}
+
+	// Après avoir exécuté un enfant, on revient au parent
+	return BTSpecialChild::ReturnToParent;
+}
