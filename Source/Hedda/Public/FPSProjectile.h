4e5a4006afa157d53e57086eb00fed956129da6f
0shiros <133508599+0shiros@users.noreply.github.com>
2025-05-28 10:51:17 +0200

    Add size modifier to bullet (#27)

--- Source/Hedda/Public/FPSProjectile.h
+++ Source/Hedda/Public/FPSProjectile.h
@@ -43,6 +43,9 @@ public:
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Caracteristics")
 	float damage = 20.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Caracteristics")
+	float size = 10.0f;
     
     // Function that is called when the projectile hits something.
     UFUNCTION(BlueprintCallable, Category = "Rotation")
