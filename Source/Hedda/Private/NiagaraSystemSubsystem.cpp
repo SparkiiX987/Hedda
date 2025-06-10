115076a66b3a6f315b7b13eec5211eb8417a0e7a
LucyanaPanda <lucie.nguyenpham@gmail.com>
2025-06-10 20:18:20 +0200

    BP manager working again

--- Source/Hedda/Private/NiagaraSystemSubsystem.cpp
+++ Source/Hedda/Private/NiagaraSystemSubsystem.cpp
@@ -4,11 +4,19 @@
 
 void UNiagaraSystemSubsystem::PlayParticle(FName _particleName, FVector _location, FRotator _rotation, USceneComponent* _parentComponent)
 {
-	if (!ParticleBank || !_parentComponent)
+	if (!ParticleBank)
 	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, FString::Printf(TEXT("The bank is missing")));
 		return;
 	}
 
+	if (!_parentComponent)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, FString::Printf(TEXT("The _parent component is missing")));
+		return;
+	}
+
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, FString::Printf(TEXT("Getting the NE")));
 	UNiagaraSystem* particule = ParticleBank->GetParticuleByName(_particleName);
 	if (particule)
 	{
@@ -21,5 +29,6 @@ void UNiagaraSystemSubsystem::PlayParticle(FName _particleName, FVector _locatio
 			EAttachLocation::KeepRelativeOffset,
 			true                                // AutoDestroy
 		);
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, FString::Printf(TEXT("Played the NE")));
 	}
 }
