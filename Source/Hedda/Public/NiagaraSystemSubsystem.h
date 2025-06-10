a3cc42b7578ad635229a87595b553d46e0c892dd
LucyanaPanda <lucie.nguyenpham@gmail.com>
2025-06-10 16:11:05 +0200

    Starting implementing VFX

--- Source/Hedda/Public/NiagaraSystemSubsystem.h
+++ Source/Hedda/Public/NiagaraSystemSubsystem.h
@@ -18,6 +18,7 @@ class HEDDA_API UNiagaraSystemSubsystem : public UGameInstanceSubsystem
 	GENERATED_BODY()
 	
 	public :
+
 		UFUNCTION(BlueprintCallable, Category = "NiagaraSystem")
 		void PlayParticle(FName _particleName, FVector _location, FRotator _rotation, USceneComponent* _parentComponent);
 		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "NiagaraSystem")
